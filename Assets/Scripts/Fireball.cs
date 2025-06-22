83fde2d09ffe637e0cd49f863869ffbb99d258c6
evemiz <evemizrahi2@gmail.com>
2025-06-22 14:16:59 +0300

    if the firball collides with the environment - destroy it

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -20,4 +20,14 @@ public class Fireball : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("Player") || other.CompareTag("AI"))
+    {
+        return;
+    }
+
+    Destroy(gameObject);
+}
 }
